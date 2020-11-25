ae10659ac2b99c73cdfd5868289922ab6a4c5bfe
JetBape <donakada8963@gmail.com>
2020-11-25 19:37:55 +0700

    Target

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import pygame
 
 try:
-    x, n = map(int, input().split())
+    w, n = map(int, input().split())
 except Exception:
     print('Неправильный формат ввода')
     exit(0)
 
 
 def draw():
-    screen.fill((255, 255, 255))
-    cnt = x / n
+    screen.fill((0, 0, 0))
     color = ()
-    for i in range(n):
-        if i % 2 == 0:
-            color = (0, 0, 0)
-            for j in range(n):
-                if i % 2 == 0:
-                    color = (0, 0, 0)
-                    pygame.draw.rect(screen, )
+    for i in range(1, n + 1):
+        if i % 2 == 0 or i == 1:
+            color = (255, 0, 0)
+        elif i % 3 == 0:
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
 
+        pygame.draw.circle(screen, color, (w * n, w * n), (n + 1 - i) * w)
 
 
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     # инициализация Pygame:
     pygame.init()
     # размеры окна:
-    size = width, height = x, x
+    size = width, height = w * n * 2, w * n * 2
     # screen — холст, на котором нужно рисовать:
     screen = pygame.display.set_mode(size)
     draw()
