d663fd6b737e80cce0d05917cd912aa050b0dcf9
JetBape <donakada8963@gmail.com>
2020-11-25 19:20:49 +0700

    Chess

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-import sys
+import pygame
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt
+try:
+    x, n = map(int, input().split())
+except Exception:
+    print('Неправильный формат ввода')
+    exit(0)
 
 
-class RandomFigures(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def draw():
+    screen.fill((255, 255, 255))
+    cnt = x / n
+    color = ()
+    for i in range(n):
+        if i % 2 == 0:
+            color = (0, 0, 0)
+            for j in range(n):
+                if i % 2 == 0:
+                    color = (0, 0, 0)
+                    pygame.draw.rect(screen, )
 
-    def paintEvent(self, event):
+
+
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # размеры окна:
+    size = width, height = x, x
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    draw()
+    # формирование кадра:
+    # команды рисования на холсте
+    # ...
+    # ...
+    # смена (отрисовка) кадра:
+    pygame.display.set_caption('Шахматная клетка')
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.flip()
+    # завершение работы:
+    pygame.quit()
